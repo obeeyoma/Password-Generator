e9ec10a3983d60ebe8e690dc54150e7cff842701
Obioma <obeeomaa@gmail.com>
2023-11-27 22:18:10 +0100

    Add logic for getRandom function

--- assets/js/script.js
+++ assets/js/script.js
@@ -129,7 +129,9 @@ function getPasswordOptions() {
 
 // Function for getting a random element from an array
 function getRandom(arr) {
-
+var selectedCharcters = "";
+  selectedCharcters+= arr[Math.floor(Math.random() * arr.length)];
+  return selectedCharcters;
 }
 
 // Function to generate password with user input
